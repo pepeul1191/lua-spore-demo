6a0fd6cc73ec9535add7e1834ab62d8d98041010
Pepe Valdivia <jvaldivia@softweb.pe>
2018-03-16 08:47:21 -0500

    Usando spore con url params

--- app.lua
+++ app.lua
@@ -1,20 +1,27 @@
 local spore = require("Spore")
 
 local client = spore.new_from_lua{
-    base_url = 'http://localhost:8888/',
-    methods = {
-        departamento_listar = {
-            path = '/departamento/listar',
-            method = 'GET',
-        },
-        distrito_buscar = {
-            path = '/distrito/buscar',
-            method = 'GET',
-            required_params = {
-                'nombre',
-            },
-        },
+  base_url = 'http://localhost:8888/',
+  methods = {
+    departamento_listar = {
+      path = '/departamento/listar',
+      method = 'GET',
     },
+    distrito_buscar = {
+      path = '/distrito/buscar',
+      method = 'GET',
+      required_params = {
+        'nombre',
+      },
+    },
+    distrito_listar = {
+      path = '/distrito/listar/:provincia_id',
+      method = 'GET',
+      required_params = {
+        'provincia_id',
+      },
+    },
+  },
 }
 
 local req1 = client:departamento_listar{}
@@ -22,3 +29,6 @@ print(req1.body)
 print("-----------------------------------------")
 local req2 = client:distrito_buscar{ nombre = 'La V'}
 print(req2.body)
+print("-----------------------------------------")
+local req3 = client:distrito_listar{ provincia_id = '2'}
+print(req3.body)
